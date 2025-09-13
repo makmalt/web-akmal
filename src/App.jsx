27497ed28cf06b5db88ade091a47a25351840c32
Akmal. <makmalthoriq31@gmail.com>
2025-09-13 14:51:44 +0700

    Add Particles component for animated background, update package dependencies, and remove daisyui plugin from styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,42 @@ import NavbarComponent from "./component/NavbarComponent";
 import FooterComponent from "./component/FooterComponent";
 import Project from "./component/Project";
 import Contact from "./component/Contact";
+import Particles from "./elements/Particles";
 function App() {
   return (
     <>
-      <Hero></Hero>
+      <Hero />
       <NavbarComponent />
-      <div id="about" className="md:mt-2 md:min-h-screen">
-        <IsiBio></IsiBio>
+      <div className="relative min-h-screen">
+        {/* Particles background */}
+        <div className="absolute inset-0 -z-10">
+          <Particles
+            particleColors={["#ffffff", "#ffffff"]}
+            particleCount={800}
+            particleSpread={10}
+            speed={0.1}
+            particleBaseSize={100}
+            moveParticlesOnHover={false}
+            alphaParticles={false}
+            disableRotation={false}
+          />
+        </div>
+
+        {/* Konten setelah Hero */}
+        <div id="about" className="md:mt-2 md:min-h-screen">
+          <IsiBio />
+        </div>
+        <Milestones />
+        <div id="portfolio" className="md:mt-0 lg:mt-0 min-h-screen">
+          <Project />
+        </div>
+        <div id="skills" className="md:mt-10 lg:mt-0 min-h-auto mb-5">
+          <IsiSkills />
+        </div>
+        <div id="contact" className="md:mt-10 lg:mt-0 min-h-auto mb-5">
+          <Contact />
+        </div>
       </div>
-      <Milestones></Milestones>
-      <div id="portfolio" className="md:mt-0 lg:mt-0 min-h-screen">
-        <Project />
-      </div>
-      <div id="skills" className="md:mt-10 lg:mt-0 min-h-auto mb-5">
-        <IsiSkills />
-      </div>
-      <div id="contact" className="md:mt-10 lg:mt-0 min-h-auto mb-5">
-        <Contact />
-      </div>
-      <FooterComponent />
     </>
   );
 }
