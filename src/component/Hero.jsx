cf9d6fb3b5ec58ddc76720e05f504be12cd45517
makmalt <makmalthoriq31@gmail.com>
2025-12-02 23:24:50 +0700

    fix: change hero section

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,24 +1,15 @@
-import TextPressure from "../elements/TextPressure";
-import Background from "./Background";
-const Hero = () => {
+import { BackgroundLines } from "../components/ui/background-lines";
+
+export default function Hero() {
   return (
-    <div className="hidden md:justify-center md:flex flex-col min-h-screen items-center">
-      <Background></Background>
-      <div style={{ position: "relative", height: "300px" }}>
-        <TextPressure
-          text="Ahlan"
-          flex={true}
-          alpha={false}
-          stroke={false}
-          width={true}
-          weight={true}
-          italic={true}
-          textColor="#ffffff"
-          strokeColor="#ff0000"
-          minFontSize={250}
-        />
-      </div>
-    </div>
+    <BackgroundLines className="flex items-center justify-center w-full flex-col px-4" svgOptions={{duration: 8}}>
+      <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-600 to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+        Akmal's, <br /> Portfolio Web.
+      </h2>
+      <p className="max-w-xl mx-auto text-sm md:text-lg text-neutral-400 text-center">
+        Welcome to my portfolio website! This is my personal website, hope you
+        enjoy it.
+      </p>
+    </BackgroundLines>
   );
-};
-export default Hero;
+}
